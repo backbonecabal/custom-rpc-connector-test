9f39a6b369e47e84e190e0be8583fd2531c66fa5
sam <sam@freighttrust.com>
2021-03-15 17:40:53 -0700

    netlify
    Signed-off-by: sam <sam@freighttrust.com>

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Backbone Cabal Page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Backbone Cabal/i);
   expect(linkElement).toBeInTheDocument();
 });
